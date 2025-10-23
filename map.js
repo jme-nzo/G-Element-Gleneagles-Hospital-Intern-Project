479709615da3bc5a540a2170c35b03b9192f49d1
jaimejover <jaimejover8@gmail.com>
2025-10-23 17:17:41 +0800

    Update map.js

--- map.js
+++ map.js
@@ -1,1 +1,9 @@
+import maplibregl from 'maplibre-gl';
+import 'maplibre-gl/dist/maplibre-gl.css';
 
+const map = new maplibregl.Map({
+    container: 'map', // container ID
+    style: 'https://demotiles.maplibre.org/style.json', // style URL
+    center: [-74.5, 40], // starting position [lng, lat]
+    zoom: 9 // starting zoom
+});
